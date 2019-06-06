c90e9fbf2efeb52b238509ec2a2ab9ba6688733f
Yago Henrique Ferreira <yagoferreira21@gmail.com>
2019-06-06 13:55:15 -0300

    Testes do injetor de dependencias

---
+++ src/backend/Yagohf.Gympass.RaceAnalyser.UnitTests/Infrastructure/InjectorBootstrapperTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yagohf.Gympass.RaceAnalyser.Injector;
+
+namespace Yagohf.Gympass.RaceAnalyser.UnitTests.Infrastructure
+{
+    [TestClass]
+    public class InjectorBootstrapperTests
+    {
+        [TestMethod]
+        public void Testar_RegisterServices()
+        {
+            //Arrange.
+            IServiceCollection serviceCollection = new ServiceCollection();
+
+            //Act.
+            InjectorBootstrapper.RegisterServices(serviceCollection, TestUtil.GetConfiguration());
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            //Assert.
+            Assert.IsNotNull(serviceProvider);
+        }
+    }
+}
---
+++ src/backend/Yagohf.Gympass.RaceAnalyser.UnitTests/TestUtil.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Yagohf.Gympass.RaceAnalyser.UnitTests
+{
+    public static class TestUtil
+    {
+        private static IConfiguration _configuration;
+
+        static TestUtil()
+        {
+            _configuration = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetCurrentDirectory())
+                      .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                      .AddEnvironmentVariables()
+                      .Build();
+        }
+
+        public static IConfiguration GetConfiguration()
+        {
+            return _configuration;
+        }
+    }
+}
