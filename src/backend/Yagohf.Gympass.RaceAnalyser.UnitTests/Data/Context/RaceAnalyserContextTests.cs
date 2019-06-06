14019c5126056d09eb07dc4368b96b27b88444a6
Yago Henrique Ferreira <yagoferreira21@gmail.com>
2019-06-06 12:41:10 -0300

    Testes unitarios do contexto do ORM

---
+++ src/backend/Yagohf.Gympass.RaceAnalyser.UnitTests/Data/Context/RaceAnalyserContextTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yagohf.Gympass.RaceAnalyser.Data.Context;
+using Yagohf.Gympass.RaceAnalyser.Model.Entities;
+
+namespace Yagohf.Gympass.RaceAnalyser.UnitTests.Data.Context
+{
+    [TestClass]
+    public class RaceAnalyserContextTests
+    {
+        [TestMethod]
+        public void Testar_Criacao()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<RaceAnalyserContext>()
+               .UseInMemoryDatabase(databaseName: "DB_CREATION_TEST")
+               .Options;
+
+            //Act.
+            RaceAnalyserContext context = new RaceAnalyserContext(options);
+            var setRaces = context.Set<Race>(); //Forçar a inicialização (OnModelCreating)
+
+            //Assert.
+            Assert.IsNotNull(context);
+            Assert.IsNotNull(setRaces);
+        }
+    }
+}
